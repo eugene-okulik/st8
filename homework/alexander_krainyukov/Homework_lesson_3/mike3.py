11e7bcea44ff6b458e5e1e3d9247e8c0f1d06926
Alex K <alexsanremo@yandex.ru>
2024-07-08 01:26:59 +0300

    Homework lesson 3 Done

---
+++ homework/alexander_krainyukov/Homework_lesson_3/mike3.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+a = 10
+b = 5
+
+# Среднее арифметическое
+avg_arithmetic = (a + b) / 2
+print("Среднее арифметическое:", avg_arithmetic)
+
+# Среднее геометрическое
+
+avg_geometric = sqrt(a * b)
+print("Среднее геометрическое:", avg_geometric)
