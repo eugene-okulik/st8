a58dca8aef967272f81e6144d1c17e1d689386c9
Margarita Liaushkina <margarita.liaushkina@balkantel.net>
2024-07-08 16:53:56 +0300

    added homework_3

---
+++ homework/margarita_liayshkina/homework_3/lesson_3_0.py
@@ -0,0 +1,16 @@
+# Даны 2 числа a и b. Получить их сумму, разность и произведение
+
+a = 2
+b = 4
+
+result_sum = a + b
+print(f"Sum: {result_sum}")
+
+result_subs = a - b
+print(f"Subtraction: {result_subs}")
+
+result_product = a * b
+print(f"Product: {result_product}")
+
+
+
---
+++ homework/margarita_liayshkina/homework_3/lesson_3_1.py
@@ -0,0 +1,7 @@
+# Даны числа x и y. Получить x − y / 1 + xy
+
+x = 3
+y = 5
+
+result = (x- y)/(1 + x * y)
+print(f"Result: {result}")
