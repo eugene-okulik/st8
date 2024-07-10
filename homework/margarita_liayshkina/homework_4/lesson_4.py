b5afa7ca4361658fbf49b775638dd1fda8961946
Margarita Liaushkina <margarita.liaushkina@balkantel.net>
2024-07-10 17:09:57 +0300

    fixed extra space

--- homework/margarita_liayshkina/homework_4/lesson_4.py
+++ homework/margarita_liayshkina/homework_4/lesson_4.py
@@ -12,7 +12,7 @@ desired_acquisition = input("What do you want to buy?: ")
 cost = float(input("How match does it cost?: "))
 your_many = float(input("How match many do you have?: "))
 monthly_savings = float(input("How much money can you save per month?: "))
-money_need =  cost - your_many
+money_need = cost - your_many
 months_to_save = money_need / monthly_savings
 
 print(f"Hello, {your_name}. You are short of {money_need} for the purchase to {desired_acquisition} ")
