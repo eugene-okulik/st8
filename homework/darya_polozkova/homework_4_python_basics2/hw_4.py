3f36cd0efdeff825ab0fa0b5e7d4e8e7a3340526
darya.palazkova <darya.palazkova@viber.com>
2024-07-10 17:55:26 +0300

    fixed lint: added spaces in ariyhmetics

--- homework/darya_polozkova/homework_4_python_basics2/hw_4.py
+++ homework/darya_polozkova/homework_4_python_basics2/hw_4.py
@@ -4,13 +4,10 @@ price = float(input("Сколько это стоит? "))
 money = float(input("Сколько у вас есть? "))
 debt = float(input("Сколько можете отложить в месяц? "))
 
-needed = price-money
-months = needed/debt
-
 aim = (
     f"Привет, {name}. На твою мечту - {dream} не хватает {price-money}. "
-       f"Можно ли купить сейчас? - Ответ: {money == price}. До исполнения мечты осталось месяцев: {needed/debt}"
+    f"Можно ли купить сейчас? - Ответ: {money == price}. "
+    f"До исполнения мечты осталось месяцев: {(price - money) / debt}"
        )
 
 print(aim)
-
