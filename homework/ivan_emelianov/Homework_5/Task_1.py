2d728a86742873140921fd47e0f6c9fbe5433946
IvanEmelianov <64010883+IvanEmelianov@users.noreply.github.com>
2024-07-16 13:29:29 +0200

    added homework task_1

---
+++ homework/ivan_emelianov/Homework_5/Task_1.py
@@ -0,0 +1,31 @@
+my_dict = {
+    'tuple': (12, 234, 44, 6, "qw", 1),
+    'list': [45, 80, 34, "rt", 12],
+    'set': {1, 3, 54, "add", 0},
+    'dist': {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5'
+    }
+}
+
+# Взаимодействие с тем, что хранится под ключом ‘tuple’
+print(my_dict['tuple'][-1])
+
+# Взаимодействие с тем, что хранится под ключом ‘list’
+my_dict['list'].append(18)
+my_dict['list'].pop(1)
+
+# Взаимодействие с тем, что хранится под ключом ‘set’
+my_dict['set'].add(15)
+my_dict['set'].remove("add")
+
+# Взаимодействие с тем, что хранится под ключом ‘dict’
+my_dict['dist']['i am a tuple'] = 'Im not a tuple, Im a dictionary'
+del my_dict['dist']['two']
+
+print(my_dict['list'])
+print(my_dict['dist'])
+print(my_dict['set'])
