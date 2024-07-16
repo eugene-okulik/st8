ebf7613365acf8b90c94b61bf9a556bab1e65ded
Ilia <118597483+IlyaPashaevQA@users.noreply.github.com>
2024-07-16 17:38:29 +0300

    lint fix2

--- homework/ilya_pashaev/Homework_5/homework5 task1.py
+++ homework/ilya_pashaev/Homework_5/homework5 task1.py
@@ -1,10 +1,10 @@
 
 
 my_dict = {
-    'tuple': (1, 2, 3, 4, 5),
-    'list': [10, 11, 12, 13, 14],
-    'dict': {'a' : 100, 'b' : 200, 'c' : 300, 'd' : 400, 'e' : 500},
-    'set': {1, 2, 3, 4, 5}
+    'tuple' : (1, 2, 3, 4, 5),
+    'list' : [10, 11, 12, 13, 14],
+    'dict' : {'a' : 100, 'b' : 200, 'c' : 300, 'd' : 400, 'e' : 500},
+    'set' : {1, 2, 3, 4, 5}
 }
 print('Before')
 print(my_dict['tuple'])
@@ -12,7 +12,7 @@ print(my_dict['list'])
 print(my_dict['dict'])
 print(my_dict['set'])
 print('After')
-print('Last tuple element:', my_dict['tuple'][-1])
+print('Last tuple element: ', my_dict['tuple'][-1])
 my_dict['list'].append(16)
 del my_dict['list'][1]
 my_dict['dict'][('i am a tuple')] = 1000
