740ff7b947b5933776ea76b1c171d75c3672c8a9
Ilia <118597483+IlyaPashaevQA@users.noreply.github.com>
2024-07-16 17:29:16 +0300

    homework5 fiinished

---
+++ homework/ilya_pashaev/Homework_5/homework5 task2.py
@@ -0,0 +1,7 @@
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+name, last_name, city, phone, country = person
+print(name)
+print(last_name)
+print(city)
+print(phone)
+print(country)
