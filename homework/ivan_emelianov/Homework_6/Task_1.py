4528a74bcfd81deece657f704219a97df74d0445
IvanEmelianov <64010883+IvanEmelianov@users.noreply.github.com>
2024-07-17 20:25:32 +0200

    add tast_1

---
+++ homework/ivan_emelianov/Homework_6/Task_1.py
@@ -0,0 +1,20 @@
+operation_1 = 'результат операции: 42'
+operation_2 = 'результат операции: 514'
+app = 'результат работы программы: 9'
+
+search = operation_1.find(': ')
+search_app = app.find(': ')
+
+result_operation_1 = operation_1[search:]
+result_operation_2 = operation_2[search:]
+result_app = app[search_app:]
+
+result_operation_1 = result_operation_1.lstrip(' :')
+result_operation_2 = result_operation_2.lstrip(' :')
+result_app = result_app.lstrip(' :')
+
+print(
+    f'результат операции: {result_operation_1 } \n'
+    f'результат операции: {result_operation_2 } \n'
+    f'результат работы программы: {result_app }'
+)
