3af1d479d68f715a2b5223e54aa0b0eac79eaa38
istrybuk <79363807+istrybuk@users.noreply.github.com>
2024-07-22 22:23:14 +0300

    fixed, method for getting number from  string

--- homework/ivan_strybuk/homework_6/task_1.py
+++ homework/ivan_strybuk/homework_6/task_1.py
@@ -3,9 +3,9 @@ text_1 = "результат операции: 514"
 text_2 = "результат работы программы: 9"
 summand = 10
 
-split_text = text.split(': ')[1]
-split_text_1 = text_1.split(': ')[1]
-split_text_2 = text_2.split(': ')[1]
+split_text = text[text.find(":") + 2::]
+split_text_1 = text_1[text_1.find(":") + 2::]
+split_text_2 = text_2[text_2.find(":") + 2::]
 
 expression_0 = int(split_text) + summand
 expression_1 = int(split_text_1) + summand
@@ -14,7 +14,6 @@ expression_2 = int(split_text_2) + summand
 print(f"Есть текст '{text}'. К числу {split_text} + {summand} =", expression_0)
 print(f"Есть текст '{text_1}'. К числу {split_text_1} + {summand} =", expression_1)
 print(f"Есть текст '{text_2}'. К числу {split_text_2} + {summand} =", expression_2)
-
 
 # def parser_number(some_pages: str) -> int:
 #     number_to_add = 10
