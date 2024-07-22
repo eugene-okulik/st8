24edec888cefcaf80b04103bd25fcad739b20409
darya.palazkova <darya.palazkova@viber.com>
2024-07-22 19:37:32 +0300

    fixed variable name in part_1_hw

--- homework/darya_polozkova/homework_7_conditions/hw_7_part_1.py
+++ homework/darya_polozkova/homework_7_conditions/hw_7_part_1.py
@@ -1,4 +1,4 @@
-text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
+text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel."
         "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
 words = text.split(' ')
 print(words)
