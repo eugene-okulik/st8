f810b97275479ab11d6f4347a55341b81c8a9cd2
Alex K <alexsanremo@yandex.ru>
2024-07-22 23:03:46 +0300

    fixed string number 8

--- homework/alexander_krainyukov/Homework_lesson_5/Collections.py
+++ homework/alexander_krainyukov/Homework_lesson_5/Collections.py
@@ -5,7 +5,7 @@ my_dict['list'].append(777)
 my_dict['list'].pop(1)
 print(my_dict['list'])
 my_dict['dict'].pop('1')
-my_dict['dict']['i_am_a_tuple'] = 333
+my_dict['dict']['i_am_a_tuple',] = ''
 my_dict['set'].add(7)
 my_dict['set'].pop()
 print(my_dict)
