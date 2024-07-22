3c826e31ec931f3bb17f1da69d5f2790314d3464
darya.palazkova <darya.palazkova@viber.com>
2024-07-22 19:52:52 +0300

    lint check

--- homework/darya_polozkova/homework_7_conditions/hw_7_part_1.py
+++ homework/darya_polozkova/homework_7_conditions/hw_7_part_1.py
@@ -1,11 +1,11 @@
-text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel."
+text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
         "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
 words = text.split(' ')
 print(words)
 new_words = []
 
 for word in words:
-    if word.endswith((",", ".")):
+    if word.endswith((",",   ".")):
         word = word[:-1] + "ing" + word[-1]
     else:
         word = word + "ing"
