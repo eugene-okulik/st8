468574cd411e0f3ccc40aec63bbeca2c2f84d77d
Alex K <alexsanremo@yandex.ru>
2024-07-22 16:30:20 +0300

    Mikoshi_homework_lesson_5_done

---
+++ homework/alexander_krainyukov/Homework_lesson_5/Collections_2.py
@@ -0,0 +1,7 @@
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+name, last_name, city, phone, country = person
+print(f'name: {name}')
+print(f'lastname: {last_name}')
+print(f'city: {city}')
+print(f'phone: {phone}')
+print(f'country: {country}')
