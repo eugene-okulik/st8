16f37c4afcc9edc2f0783e20a4db5093b36660d9
Alex K <alexsanremo@yandex.ru>
2024-07-25 13:45:40 +0300

    fixed location variables

--- homework/alexander_krainyukov/Homework_lesson_7/Programm_v2.py
+++ homework/alexander_krainyukov/Homework_lesson_7/Programm_v2.py
@@ -1,10 +1,10 @@
 my_range = range(1, 101)
 for num in my_range:
-    if num % 3 == 0:
-        print('Fuzz')
+    if num % 3 == 0 and num % 5 == 0:
+        print('FuzzBuzz')
     elif num % 5 == 0:
         print('Buzz')
-    elif num % 3 == 0 and num % 5 == 0:
-        print('FuzzBuzz')
+    elif num % 3 == 0:
+        print('Fuzz')
     else:
         print(num)
