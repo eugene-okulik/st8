591175e80e0bdd111b23c93f542b6b4706f1ac1a
darya.palazkova <darya.palazkova@viber.com>
2024-07-26 14:16:55 +0300

    fix lint

--- homework/darya_polozkova/homework_8_while_and_functions/hw_8_part_3.py
+++ homework/darya_polozkova/homework_8_while_and_functions/hw_8_part_3.py
@@ -3,10 +3,12 @@ operation_2 = 'результат операции: 54'
 operation_3 = 'результат работы программы: 209'
 operation_4 = 'результат: 2'
 
+
 def calculate(operation):
     start = operation.index(': ')
     result = int(operation[start:].strip(': ')) + 10
     print(result)
+
 
 calculate(operation_1)
 calculate(operation_2)
