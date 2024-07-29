527518ad7848da07abc18ff17a60c3e9ad54c526
IvanEmelianov <64010883+IvanEmelianov@users.noreply.github.com>
2024-07-29 22:03:47 +0200

    homework_9

---
+++ homework/ivan_emelianov/Homework_9/Task_1.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.set_int_max_str_digits(0)
+
+
+def fibonacci_generator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+fib_gen = fibonacci_generator()
+
+fifth_number = None
+for i in range(5):
+    fifth_number = next(fib_gen)
+print(f'Пятое число Фибоначчи: {fifth_number}')
+
+fib_gen = fibonacci_generator()
+for i in range(200):
+    two_hundredth_number = next(fib_gen)
+print(f'Двухсотое число Фибоначчи: {two_hundredth_number}')
+
+fib_gen = fibonacci_generator()
+for i in range(1000):
+    thousandth_number = next(fib_gen)
+print(f'Тысячное число Фибоначчи: {thousandth_number}')
+
+fib_gen = fibonacci_generator()
+for i in range(100000):
+    hundred_thousandth_number = next(fib_gen)
+print(f'Стотысячное число Фибоначчи: {hundred_thousandth_number}')
