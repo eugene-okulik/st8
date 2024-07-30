769428a9388ba9808a4eaf79b05881c676495a0f
darya.palazkova <darya.palazkova@viber.com>
2024-07-30 15:30:31 +0300

    упс))это из-за растроенности в себе

---
+++ homework/darya_polozkova/homework_9_functions_and_generators/hw_1.py
@@ -0,0 +1,14 @@
+import sys
+sys.set_int_max_str_digits(0)
+def fibonacci():
+    fib1, fib2 = 0, 1
+    while True:
+        fib1, fib2 = fib2, fib1 + fib2
+        yield fib1
+
+for num, val in enumerate(fibonacci()):
+    number = num + 1
+    nums = [5, 200, 1000, 100000]
+    for i in nums:
+        if number == i:
+            print(f"This is {num} key in Fibonacci with {val}")
