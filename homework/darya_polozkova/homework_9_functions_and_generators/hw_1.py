fac7639da83beac13db7fafaa8a87951e728046d
darya.palazkova <darya.palazkova@viber.com>
2024-07-30 15:42:46 +0300

    added lint fixes and break for ending cycle

--- homework/darya_polozkova/homework_9_functions_and_generators/hw_1.py
+++ homework/darya_polozkova/homework_9_functions_and_generators/hw_1.py
@@ -1,18 +1,17 @@
 import sys
+
 sys.set_int_max_str_digits(0)
+
 def fibonacci():
-
-
     fib1, fib2 = 0, 1
     while True:
         fib1, fib2 = fib2, fib1 + fib2
         yield fib1
 
 for num, val in enumerate(fibonacci()):
-
-
     number = num + 1
     nums = [5, 200, 1000, 100000]
     for i in nums:
         if number == i:
-            print(f"This is {num} key in Fibonacci with {val}")
+            print(f"This is {num} key in Fibonacci with {val} value")
+            break
