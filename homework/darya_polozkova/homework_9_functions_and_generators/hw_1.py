f063c63c5cb643ff5cac39f9faafd3d9cb87fd56
darya.palazkova <darya.palazkova@viber.com>
2024-07-30 15:44:19 +0300

    I hope final lint

--- homework/darya_polozkova/homework_9_functions_and_generators/hw_1.py
+++ homework/darya_polozkova/homework_9_functions_and_generators/hw_1.py
@@ -2,11 +2,13 @@ import sys
 
 sys.set_int_max_str_digits(0)
 
+
 def fibonacci():
     fib1, fib2 = 0, 1
     while True:
         fib1, fib2 = fib2, fib1 + fib2
         yield fib1
+
 
 for num, val in enumerate(fibonacci()):
     number = num + 1
