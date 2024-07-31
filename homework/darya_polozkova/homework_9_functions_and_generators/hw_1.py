32ec05337423ef9b6f35c9880d90e8ff57dabf08
darya.palazkova <darya.palazkova@viber.com>
2024-07-31 09:06:09 +0300

    fixed after comments on lesson: moved yield and changed counting of nums

--- homework/darya_polozkova/homework_9_functions_and_generators/hw_1.py
+++ homework/darya_polozkova/homework_9_functions_and_generators/hw_1.py
@@ -6,14 +6,16 @@ sys.set_int_max_str_digits(0)
 def fibonacci():
     fib1, fib2 = 0, 1
     while True:
-        fib1, fib2 = fib2, fib1 + fib2
         yield fib1
+        fib1, fib2 = fib2, fib1 + fib2
 
 
+nums = [5, 200, 1000, 100000]
+
+num = 0
 for num, val in enumerate(fibonacci()):
-    number = num + 1
-    nums = [5, 200, 1000, 100000]
-    for i in nums:
-        if number == i:
-            print(f"This is {num} key in Fibonacci with {val} value")
-            break
+    if num in nums:
+        print(f"This is {num} key in Fibonacci with {val} value")
+    num += 1
+    if num > 100000:
+        break
