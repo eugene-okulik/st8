be9af121b2c8d569e9f354a1bb76a08d33f49885
istrybuk <79363807+istrybuk@users.noreply.github.com>
2024-08-03 14:56:04 +0300

    fixed code line for bug correction E999 SyntaxError: f-string

--- homework/ivan_strybuk/homework_11/task_1.py
+++ homework/ivan_strybuk/homework_11/task_1.py
@@ -12,7 +12,6 @@ my_date = "Jan 15, 2023 - 12:05:33"
 
 python_date = datetime.datetime.strptime(my_date, '%b %d, %Y - %H:%M:%S')
 
-print(f"Полное название месяца: {"0" + str(python_date.date().month)}")
 print(f"Полное название месяца: {python_date.strftime("%m")}")
 print(f"Полное название месяца: {python_date.strftime("%b")}")
 print(f"Полное название месяца: {python_date.strftime("%B")}")
