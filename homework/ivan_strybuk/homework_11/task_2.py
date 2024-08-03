ab462a2427e7297274cecc93222af87955972c1c
istrybuk <79363807+istrybuk@users.noreply.github.com>
2024-08-03 23:33:55 +0300

    Исправил вывод согласно заданию {ключ: int(значение)}

--- homework/ivan_strybuk/homework_11/task_2.py
+++ homework/ivan_strybuk/homework_11/task_2.py
@@ -6,13 +6,13 @@ PRICE_LIST = '''тетрадь 50р
 пенал 300р
 рюкзак 500р'''
 
-t = dict(zip(PRICE_LIST.split()[::2], PRICE_LIST.split()[1::2]))
-print(t)
+t = dict(zip(PRICE_LIST.split()[::2], [int(cost.rstrip('р')) for cost in PRICE_LIST.split()[1::2]]))
+print(t)  # {'тетрадь': 50, 'книга': 200, 'ручка': 100, 'карандаш': 70, 'альбом': 120, 'пенал': 300, 'рюкзак': 500}
 # ---
 
 a1 = dict(zip([key for val, key in enumerate(PRICE_LIST.split()) if val % 2 == 0],
-              [value for val, value in enumerate(PRICE_LIST.split()) if val % 2 != 0]))
-print(a1)
+              [int(str(value).rstrip("р")) for val, value in enumerate(PRICE_LIST.split()) if val % 2 != 0]))
+print(a1)  # {'тетрадь': 50, 'книга': 200, 'ручка': 100, 'карандаш': 70, 'альбом': 120, 'пенал': 300, 'рюкзак': 500}
 
 # ---
 key_d = []
@@ -22,15 +22,15 @@ for x, y in enumerate(PRICE_LIST.split()):
     if x % 2 == 0:
         key_d.append(y)
     else:
-        value_d.append(y)
+        value_d.append(int(str(y).rstrip("р")))
 print(dict(zip(key_d, value_d)))
 
 
 # ---
-def Convert(a):
+def str_convert(a):
     it = iter(a)  # Получим итератор из списка "list" и выведем каждое значение:
-    res_dct = dict(zip(it, it))
+    res_dct = dict([k, int(v[:-1])] for k, v in dict(zip(it, it)).items())
     return res_dct
 
 
-print(Convert(PRICE_LIST.split()))
+print(str_convert(PRICE_LIST.split()))
