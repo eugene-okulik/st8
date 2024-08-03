e4b871f7f6ec97b4cf8be1d443dbd7c4ea710331
istrybuk <79363807+istrybuk@users.noreply.github.com>
2024-08-03 15:24:48 +0300

    fixed code line for bug correction E999 SyntaxError: f-string

--- homework/ivan_strybuk/homework_11/task_1.py
+++ homework/ivan_strybuk/homework_11/task_1.py
@@ -12,7 +12,8 @@ my_date = "Jan 15, 2023 - 12:05:33"
 
 python_date = datetime.datetime.strptime(my_date, '%b %d, %Y - %H:%M:%S')
 
-print(f"Полное название месяца: {python_date.strftime("%m")}")
-print(f"Полное название месяца: {python_date.strftime("%b")}")
-print(f"Полное название месяца: {python_date.strftime("%B")}")
-print(f'Дата в таком формате "15.01.2023, 12:05": "{python_date.strftime("%d.%m.%Y, %H:%M")}"')
+print("Полное название месяца: ", "0" + str(python_date.date().month))
+print("Полное название месяца: ", python_date.strftime("%m"))
+print("Полное название месяца: ", python_date.strftime("%b"))
+print("Полное название месяца: ", python_date.strftime("%B"))
+print('Дата в таком формате "15.01.2023, 12:05": ',  python_date.strftime("%d.%m.%Y, %H:%M"))
