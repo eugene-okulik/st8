d622b5ac7450d999590440ee2dae982921891f61
Margarita Liaushkina <margarita.liaushkina@balkantel.net>
2024-08-05 17:22:03 +0300

    home work 6

---
+++ homework/margarita_liayshkina/homework_6/lesson_6_2.py
@@ -0,0 +1,7 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+students_major = ",".join(subjects)
+lastname_stusents = ",".join(students)
+
+print(f"Students {lastname_stusents} study these subjects:{students_major}")
