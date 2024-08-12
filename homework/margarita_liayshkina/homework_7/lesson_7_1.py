b6898b75401c8f87f6a881883b203bf0de8bc13a
Margarita Liaushkina <margarita.liaushkina@balkantel.net>
2024-08-12 11:13:28 +0300

    added homework_7

---
+++ homework/margarita_liayshkina/homework_7/lesson_7_1.py
@@ -0,0 +1,25 @@
+full_text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel.\n"
+        "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero.")
+
+
+words = full_text.split()
+ls_text = []
+
+for word in words:
+    if word.endswith(','):
+        new_word = word[:-1] + 'ing' + ','
+
+    elif word.endswith('.'):
+        new_word = word[:-1] + 'ing' + '.'
+
+    else:
+        new_word = word + 'ing'
+
+    ls_text.append(new_word)
+
+new_text = ' '.join(ls_text)
+print(new_text)
+
+
+
+
