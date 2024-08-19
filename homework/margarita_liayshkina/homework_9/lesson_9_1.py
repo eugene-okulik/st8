75bf40d343b1265c71c21106bb57cd2346f63ee7
Margarita Liaushkina <margarita.liaushkina@balkantel.net>
2024-08-19 13:17:39 +0300

    fixed fib formula

--- homework/margarita_liayshkina/homework_9/lesson_9_1.py
+++ homework/margarita_liayshkina/homework_9/lesson_9_1.py
@@ -6,19 +6,19 @@ def fibonachi():
     a = 0
     b = 1
     while True:
-        fib = a + b
-        a = b
-        b = fib
-        yield fib
+        yield a
+        fib = b
+        b = a + b
+        a = fib
 
 
 wanted_numbers = [5, 200, 1000, 100000]
 last_number = wanted_numbers[len(wanted_numbers) - 1]
 print(f"last number: {last_number}")
-counter = 0
+counter = 1
 for number in fibonachi():
     if counter in wanted_numbers:
         print(number)
     counter += 1
-    if counter >= last_number:
+    if counter > last_number:
         break
