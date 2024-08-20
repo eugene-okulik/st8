ace37a6512b58271f9b379186c7dde5d9f66bc21
Margarita Liaushkina <margarita.liaushkina@balkantel.net>
2024-08-20 13:37:18 +0300

    fixed warning homework_11_1

--- homework/margarita_liayshkina/homework_11/lesson_11_1.py
+++ homework/margarita_liayshkina/homework_11/lesson_11_1.py
@@ -9,10 +9,7 @@ month_full = python_data.strftime("%B")
 print(month_full)
 
 format_data_2 = "15.01.2023, 12:05"
-python_data_2 = datetime.datetime.strptime(format_data_2,"%d.%m.%Y, %H:%M")
+python_data_2 = datetime.datetime.strptime(format_data_2, "%d.%m.%Y, %H:%M")
 # print(python_data_2)
 human_data_2 = python_data_2.strftime("%d.%m.%Y, %H:%M")
 print(human_data_2)
-
-
-
