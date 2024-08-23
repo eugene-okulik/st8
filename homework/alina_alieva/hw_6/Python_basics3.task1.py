39244587e9a37831b48d1a454d3bdaa4d1df2100
Alina Alieva <alina.alieva0101@gmail.com>
2024-08-23 20:07:38 +0200

    homework_6 done

---
+++ homework/alina_alieva/hw_6/Python_basics3.task1.py
@@ -0,0 +1,14 @@
+str1 = 'результат операции: 42'
+str2 = 'результат операции: 514'
+str3 = 'результат работы программы: 9'
+
+number1 = str1.index(':') + 1
+number2 = str2.index(':') + 1
+number3 = str3.index(':') + 1
+
+my_num1 = int(str1[number1:])
+print(my_num1 + 10)
+my_num2 = int(str2[number2:])
+print(my_num2 + 10)
+my_num3 = int(str3[number3:])
+print(my_num3 +10)
---
+++ homework/alina_alieva/hw_6/Python_basics3.task2.py
@@ -0,0 +1,6 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+text_students = ', '.join(students)
+text_subjects = ', '.join(subjects)
+print(f'Students {text_students} study these subjects: {text_subjects}')
