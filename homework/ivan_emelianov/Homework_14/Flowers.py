da992a5e8dd8902ca2cc0647dcfd0a60d0622f64
IvanEmelianov <64010883+IvanEmelianov@users.noreply.github.com>
2024-08-26 22:36:26 +0200

    home work 14

---
+++ homework/ivan_emelianov/Homework_14/Flowers.py
@@ -0,0 +1,8 @@
+class Flowers:
+    def __init__(self, name, color, smell, price=int, stem_length=int, life_time_flow=int):
+        self.name = name
+        self.color = color
+        self.smell = smell
+        self.price = price
+        self.stem_length = stem_length
+        self.life_time_flow = life_time_flow
---
+++ homework/ivan_emelianov/Homework_14/Rose.py
@@ -0,0 +1,6 @@
+from Flowers import Flowers
+
+
+class Rose(Flowers):
+    def __init__(self, name, color, smell, price=int, stem_length=int, life_time_flow=int):
+        super().__init__(name, color, smell, price, stem_length, life_time_flow)
