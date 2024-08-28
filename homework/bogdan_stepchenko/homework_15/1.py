c1cef7d65aeab2951763802a6965ad37775e9eec
b_stepchenko <b_stepchenko@wargaming.net>
2024-08-28 17:39:55 +0300

    final_polishing

--- homework/bogdan_stepchenko/homework_15/1.py
+++ homework/bogdan_stepchenko/homework_15/1.py
@@ -1,14 +1,20 @@
 import os
 from pathlib import Path
 
-repository_root = Path(os.path.dirname(__file__)).parent.parent.parent
 
-target_file = os.path.join(repository_root, 'homework', 'eugene_okulik', 'hw_13', 'data.txt')
+def print_only_upper_letters(file: str):
 
-with open(target_file, 'r', encoding='utf-8') as opened_file:
-    res = ''
-    data = opened_file.read()
-    for i in data:
-        if i.isupper():
-            res += i
-    print(f'Only upper letters from file data.txt: {res}')
+    repository_root = Path(os.path.dirname(__file__)).parent.parent.parent
+
+    target_file = os.path.join(repository_root, 'homework', 'eugene_okulik', 'hw_13', f'{file}')
+
+    with open(target_file, 'r', encoding='utf-8') as opened_file:
+        res = ''
+        data = opened_file.read()
+        for i in data:
+            if i.isupper():
+                res += i
+        print(f'Only upper letters from file data.txt: {res}')
+
+
+print_only_upper_letters('data.txt')
