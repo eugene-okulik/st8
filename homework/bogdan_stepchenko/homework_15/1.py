757b2571ef58d5c2e1a8f4bb29e6e1e235d99196
b_stepchenko <b_stepchenko@wargaming.net>
2024-08-28 17:33:35 +0300

    homework_15_is_done

---
+++ homework/bogdan_stepchenko/homework_15/1.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+repository_root = Path(os.path.dirname(__file__)).parent.parent.parent
+
+target_path = os.path.join(repository_root, 'homework', 'eugene_okulik', 'hw_13')
+
+target_file = os.path.join(target_path, 'data.txt')
+
+with open(target_file, 'r', encoding='utf-8') as opened_file:
+    res = ''
+    data = opened_file.read()
+    for i in data:
+        if i.isupper():
+            res += i
+    print(f'Only upper letters from file data.txt: {res}')
