92677382076e122e966774b3149cd4e7d944b6b9
b_stepchenko <b_stepchenko@wargaming.net>
2024-08-28 18:03:05 +0300

    removing_cycle_and_adding_filter

--- homework/bogdan_stepchenko/homework_15/1.py
+++ homework/bogdan_stepchenko/homework_15/1.py
@@ -9,12 +9,9 @@ def print_only_upper_letters(file: str):
     target_file = os.path.join(repository_root, 'homework', 'eugene_okulik', 'hw_13', f'{file}')
 
     with open(target_file, 'r', encoding='utf-8') as opened_file:
-        res = ''
         data = opened_file.read()
-        for i in data:
-            if i.isupper():
-                res += i
-        print(f'Only upper letters from file data.txt: {res}')
+        res = ''.join(filter(str.isupper, data))
+        print(f'Only upper letters from file {file}: {res}')
 
 
 print_only_upper_letters('data.txt')
