7a960a104807408e3ff1ff8c5d2f5f59415c8e98
darya.palazkova <darya.palazkova@viber.com>
2024-09-01 12:10:40 +0300

    pushed due to comments

--- homework/darya_polozkova/homework_15_tools/homework_15_tools.py
+++ homework/darya_polozkova/homework_15_tools/homework_15_tools.py
@@ -1,18 +1,14 @@
 import os
 import sys
-from pathlib import Path
 
-file_dir = Path(os.path.dirname(__file__))
-print(f'ETO{file_dir}')
 root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-print(f'ETOO{root}')
-path = os.path.join(root, 'eugene_okulik', 'hw_13')
-print(path)
+path = os.path.join(root, 'eugene_okulik', 'Lesson_15')
 data = os.path.join(path, 'data.txt')
 print(data)
 separator = '\\' if sys.platform == 'windows' else '/'
-# homework/eugene_okulik/hw_13/data.txt
+# homework/eugene_okulik/hw_15/data.txt
 with open(data, encoding='utf-8') as opened_file:
     data = opened_file.read()
     uppercase_letters = list(filter(lambda char: char.isupper(), ''.join(data)))
     print(uppercase_letters)
+
