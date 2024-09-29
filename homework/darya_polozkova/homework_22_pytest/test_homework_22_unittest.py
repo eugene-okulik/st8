47ef67fdd1017358ae5e404c627fd3e2bc65f194
darya.palazkova <darya.palazkova@gmail.com>
2024-09-29 12:11:37 +0300

    darya/homework_on_pytest_is_done

---
+++ homework/darya_polozkova/homework_22_pytest/test_homework_22_unittest.py
@@ -0,0 +1,68 @@
+import pytest
+import requests
+
+
+@pytest.fixture()
+def set_up():
+    payload = {
+        "name": "Unittest MacBook Pro 16",
+        "data": {
+            "year": 2024,
+            "price": 2000,
+            "CPU model": "M1",
+            "Hard disk size": "1 TB"
+        }
+    }
+    response = requests.post(
+        'http://167.172.172.115:52353/object',json=payload
+    )
+    object_id = response.json()['id']
+    print(f'Object for set-up is created with ID {object_id}')
+    yield object_id
+    requests.delete(f'http://167.172.172.115:52353/object/{object_id}')
+    print(f'Object for set-up is deleted with ID {object_id}')
+
+
+@pytest.fixture()
+def start_end():
+    print('Start testing')
+    yield None
+    print('Testing completed')
+
+@pytest.mark.smoke
+def test_get_object_by_id(set_up, start_end):
+    response = requests.get(f'http://167.172.172.115:52353/object/{set_up}')
+    data = response.json()
+    print(f'Your newly created object is {data['name']}')
+
+
+@pytest.mark.critical
+def test_update_object_with_put(set_up, start_end):
+    payload = {
+        "name": "Put Unittest MacBook Pro 16",
+        "data": {
+            "year": 2024,
+            "price": 2000,
+            "CPU model": "Put Intel Core i9",
+            "Hard disk size": "100 TB"
+        }
+    }
+    headers = {"Content-Type": 'application/json'}
+    response = requests.put(
+        f'http://167.172.172.115:52353/object/{set_up}',
+        json=payload,
+        headers=headers
+    )
+    print(response.json())
+
+def test_update_object_with_patch(set_up, start_end):
+    payload = {
+        "name": "Update Unittest MacBook Pro 16",
+    }
+    headers = {"Content-Type": 'application/json'}
+    response = requests.patch(
+        f'http://167.172.172.115:52353/object/{set_up}',
+        json=payload,
+        headers=headers
+    )
+    print(f'Your updated object data: {response.json()}')
