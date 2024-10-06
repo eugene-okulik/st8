827490ea75e6360eb20e01cf9fb2489984ca45fc
b_stepchenko <b_stepchenko@wargaming.net>
2024-10-06 14:44:28 +0300

    homework_26

--- api_tests_bstepchenko/conftest.py
+++ api_tests_bstepchenko/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 import requests
+
 from api_tests_bstepchenko.data import constants
+from api_tests_bstepchenko.endpoints.get_object import GetObject
+from api_tests_bstepchenko.endpoints.post_object import PostObject
+from api_tests_bstepchenko.endpoints.put_object import PutObject
+from api_tests_bstepchenko.endpoints.patch_object import PatchObject
+from api_tests_bstepchenko.endpoints.del_object import DeleteObject
+from api_tests_bstepchenko.data.randomizer import get_random_int, get_random_str
 
 
 @pytest.fixture(scope='class')
@@ -12,10 +19,42 @@ def start_end():
 
 @pytest.fixture(scope='function')
 def create_and_delete_object():
-    payload = constants.PAYLOAD_FOR_OBJECT
+    payload = {
+        "name": get_random_str(),
+        "data": {
+            "year": get_random_int(),
+            "price": get_random_int(),
+            "CPU model": get_random_str(),
+        },
+    }
     creation_response = requests.post(f'{constants.BASE_URL}',
                                       json=payload, headers=constants.HEADERS)
     created_object = creation_response.json()
     assert created_object is not None, "Failed to create the object"
-    yield created_object['id']
+    yield created_object['id'], payload
     requests.delete(f'{constants.BASE_URL}/{created_object["id"]}')
+
+
+@pytest.fixture(scope='function')
+def get_object_endpoint():
+    return GetObject()
+
+
+@pytest.fixture(scope='function')
+def post_object_endpoint():
+    return PostObject()
+
+
+@pytest.fixture(scope='function')
+def put_object_endpoint():
+    return PutObject()
+
+
+@pytest.fixture(scope='function')
+def patch_object_endpoint():
+    return PatchObject()
+
+
+@pytest.fixture(scope='function')
+def delete_object_endpoint():
+    return DeleteObject()
--- api_tests_bstepchenko/endpoints/post_object.py
+++ api_tests_bstepchenko/endpoints/post_object.py
@@ -2,14 +2,30 @@ import requests
 from api_tests_bstepchenko.data import constants
 from requests.exceptions import JSONDecodeError
 from api_tests_bstepchenko.endpoints.base_api import BaseApi
+from api_tests_bstepchenko.models.object_data import ObjectJson
 
 
 class PostObject(BaseApi):
 
-    def post_new_object(self):
-        self.payload = constants.PAYLOAD_FOR_OBJECT
-        self.response = requests.post(f'{constants.BASE_URL}', json=self.payload, headers=constants.HEADERS)
+    def post_new_object(self, payload):
+        self.response = requests.post(f'{constants.BASE_URL}', json=payload, headers=constants.HEADERS)
         try:
             self.response_json = self.response.json()
         except JSONDecodeError:
             pass
+
+    @property
+    def data(self):
+        return ObjectJson(**self.response_json)
+
+    def check_updated_name(self, payload):
+        assert self.data.name == payload['name']
+
+    def check_updated_year(self, payload):
+        assert self.data.data.year == payload['data']['year']
+
+    def check_updated_price(self, payload):
+        assert self.data.data.price == payload['data']['price']
+
+    def check_updated_cpu_model(self, payload):
+        assert self.data.data.CPU_model == payload['data']['CPU model']
