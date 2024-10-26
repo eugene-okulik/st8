f8b0cb85ee93ca0fc9a5be447f0cf85652acda64
Mikhail <>
2024-10-26 15:24:06 +0700

    homework 28 is done

---
+++ homework/mikhail_sorokin/homework_28/conftest.py
@@ -0,0 +1,14 @@
+import time
+
+from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+import pytest
+
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
