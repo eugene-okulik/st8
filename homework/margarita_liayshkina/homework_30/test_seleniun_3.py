00607099bcc34d68181186a1d514476625d31a48
admin <liaushkina83@mail.ru>
2024-11-05 16:55:11 +0200

    ready hw_selenium_3 done

---
+++ homework/margarita_liayshkina/homework_30/test_seleniun_3.py
@@ -0,0 +1,52 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.ie.webdriver import WebDriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common import TimeoutException
+
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+def test_choose_language(driver):
+    driver.get('https://www.qa-practice.com/elements/select/single_select')
+    select_language = driver.find_element(By.ID, 'id_choose_language')
+    dropdown = Select(select_language)
+    chosen_language = 'C#'
+    dropdown.select_by_visible_text('C#')
+    button = driver.find_element(By.ID, 'submit-id-submit')
+    assert button.is_enabled()
+    button.click()
+    you_selected = WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.CLASS_NAME, 'result-text'))
+    ).text
+    print(f"Chosen_language displayed correctly: {chosen_language}")
+    assert you_selected == chosen_language, f"Selected language '{chosen_language}' not displayed correctly in result got'{you_selected}'"
+
+
+def test_enter_start(driver):
+    driver.get('https://the-internet.herokuapp.com/dynamic_loading/2')
+    button_start =  WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.XPATH, "//div[@id='start']/button"))
+    )
+    assert button_start.is_enabled()
+    button_start.click()
+    try:
+        result_text = WebDriverWait(driver, 20).until(
+            ec.visibility_of_element_located((By.ID, 'finish'))
+        ).text
+    except TimeoutException:
+        assert False, "Timeout: The expected text 'Hello World!' did not appear in time"
+
+    finish_phrase = 'Hello World!'
+    print(f"Current Phrase is displayed correctly: {result_text}")
+    assert result_text == finish_phrase, f"Expected 'Hello World!' but got '{result_text}'"
