bb741d7febf7a7182ebd0561241f7f2c44693bbb
admin <liaushkina83@mail.ru>
2024-11-21 11:20:17 +0200

    remove test_lesson_31 and update test_demoblaze_1

--- homework/margarita_liayshkina/homework_31/test_demoblaze_1.py
+++ homework/margarita_liayshkina/homework_31/test_demoblaze_1.py
@@ -16,7 +16,7 @@ def driver():
     driver.quit()
 
 
-def open_product_page(driver):
+def test_demoblaze(driver):
     driver.get('https://www.demoblaze.com/index.html')
     link = WebDriverWait(driver, 10).until(
         ec.visibility_of_element_located((By.CSS_SELECTOR, "a[href='prod.html?idp_=1']"))
@@ -24,14 +24,10 @@ def open_product_page(driver):
     assert link is not None, "Product link is not visible on the main page"
     ActionChains(driver).key_down(Keys.CONTROL).click(link).key_up(Keys.CONTROL).perform()
 
-
-def switch_to_new_window(driver):
     tabs = driver.window_handles
     driver.switch_to.window(tabs[1])
     assert driver.current_url.endswith('prod.html?idp_=1'), "The new window did not open the expected product page"
 
-
-def add_product_to_cart(driver):
     addcard_button = WebDriverWait(driver, 10).until(
         ec.element_to_be_clickable((By.XPATH, "//a[@onclick='addToCart(1)']"))
     )
@@ -40,14 +36,9 @@ def add_product_to_cart(driver):
     alert = Alert(driver)
     alert.accept()
 
-
-def close_current_window_and_switch_back(driver):
     driver.close()
     tabs = driver.window_handles
     driver.switch_to.window(tabs[0])
-
-
-def go_to_cart_and_verify_product(driver):
     button_cart = WebDriverWait(driver, 20).until(
         ec.visibility_of_element_located((By.CSS_SELECTOR, "a[href='cart.html']"))
     )
@@ -55,14 +46,5 @@ def go_to_cart_and_verify_product(driver):
 
     WebDriverWait(driver, 20).until(ec.presence_of_element_located((By.XPATH, '//tr[@class="success"]')))
     expected_name_product = "Samsung galaxy s6"
-    product_cart_name = driver.find_element(By.XPATH, ".//td[text()='Samsung galaxy s6']")
-    assert product_cart_name.text == expected_name_product, \
-        f"Expected '{expected_name_product}', found '{product_cart_name.text}'"
-
-
-def test_demoblaze(driver):
-    open_product_page(driver)
-    switch_to_new_window(driver)
-    add_product_to_cart(driver)
-    close_current_window_and_switch_back(driver)
-    go_to_cart_and_verify_product(driver)
+    product_cart_name = driver.find_element(By.XPATH,  ".//td[text()='Samsung galaxy s6']")
+    assert product_cart_name.text== expected_name_product, f"Expected result '{expected_name_product}', but found {product_cart_name.text}"
