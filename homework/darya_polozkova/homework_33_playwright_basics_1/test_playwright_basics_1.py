1bd5275482e9351b2c4f5595db60523d8bba78dd
darya.palazkova <darya.palazkova@gmail.com>
2024-11-24 23:38:25 +0300

    hw_33 is done

---
+++ homework/darya_polozkova/homework_33_playwright_basics_1/test_playwright_basics_1.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Page, expect
+import re
+
+def test_get_by_role(page: Page):
+    page.goto('https://the-internet.herokuapp.com/')
+    search_field = page.get_by_role('link', name='Form Authentication')
+    search_field.press('Enter')
+    username = page.get_by_role('textbox', name='username')
+    username.fill('tomsmith')
+    username.press('Enter')
+    password = page.get_by_role('textbox', name='password')
+    password.fill('SuperSecretPassword!')
+    password.press('Enter')
+    expect(page).to_have_url('https://the-internet.herokuapp.com/login')
