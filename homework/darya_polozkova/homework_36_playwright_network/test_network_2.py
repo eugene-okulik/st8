5566da069e91aaafe795fddf5bd75ca447fde268
darya.palazkova <darya.palazkova@gmail.com>
2024-12-06 15:47:38 +0300

    lint fix

--- homework/darya_polozkova/homework_36_playwright_network/test_network_2.py
+++ homework/darya_polozkova/homework_36_playwright_network/test_network_2.py
@@ -11,10 +11,10 @@ def test_change_phone_name(page: Page):
         body['body']['digitalMat'][0]['productName'] = 'яблокофон 16 про'
         route.fulfill(response=response, json=body)
     # route именно для приостанавливания запроса и подмены
-    page.route(re.compile('/step0_iphone/digitalmat$'),change_resp)
+    page.route(re.compile('/step0_iphone/digitalmat$'), change_resp)
     page.goto('https://www.apple.com/shop/buy-iphone')
     time.sleep(10)
     page.locator('.rf-hcard-content-title').first.click()
     time.sleep(10)
     title = page.locator('.rf-digitalmat-tabnav-button.current').text_content()
-    assert title ==  'яблокофон 16 про'
+    assert title == 'яблокофон 16 про'
