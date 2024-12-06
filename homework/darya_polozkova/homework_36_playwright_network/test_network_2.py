dd45d9ace032b7661977e1f4a82f2bc726a0b083
darya.palazkova <darya.palazkova@gmail.com>
2024-12-06 15:46:03 +0300

    homework_36 is done

---
+++ homework/darya_polozkova/homework_36_playwright_network/test_network_2.py
@@ -0,0 +1,20 @@
+import time
+from playwright.sync_api import Page, Route
+import re
+
+
+def test_change_phone_name(page: Page):
+
+    def change_resp(route: Route):
+        response = route.fetch()
+        body = response.json()
+        body['body']['digitalMat'][0]['productName'] = 'яблокофон 16 про'
+        route.fulfill(response=response, json=body)
+    # route именно для приостанавливания запроса и подмены
+    page.route(re.compile('/step0_iphone/digitalmat$'),change_resp)
+    page.goto('https://www.apple.com/shop/buy-iphone')
+    time.sleep(10)
+    page.locator('.rf-hcard-content-title').first.click()
+    time.sleep(10)
+    title = page.locator('.rf-digitalmat-tabnav-button.current').text_content()
+    assert title ==  'яблокофон 16 про'
