5e557f67454cddef8cc37da469f8f140f7327d61
admin <liaushkina83@mail.ru>
2025-01-08 14:33:33 +0200

    fixed blank line

--- pw_test_mliaushkina/pages/locators/locator_customer_create.py
+++ pw_test_mliaushkina/pages/locators/locator_customer_create.py
@@ -7,5 +7,3 @@ CREATE_ACCOUNT_BUTTON = '//*[@id="form-validate"]/div/div[1]/button'
 SUCCESS_MESSAGE = '//*[text()="Thank you for registering with Main Website Store."]'
 EMAIL_ERROR = '#email_address-error'
 CONFIRM_PASSWORD_ERROR = '#password-confirmation-error'
-
-
--- pw_test_mliaushkina/pages/locators/locator_sale.py
+++ pw_test_mliaushkina/pages/locators/locator_sale.py
@@ -2,6 +2,3 @@ BREADCRUMB_ITEM = 'li[class="item category37"]'
 BLOCKS_PROMO_ITEM = 'div[class="blocks-promo"]'
 WOMAN_SALE_ITEM = 'a[class="block-promo sale-main"]'
 MAIN_SALE_TITLE_ITEM = 'strong[class="title"]'
-
-
-
